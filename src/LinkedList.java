cad8867e36d0797dce981d00a547315660da71c3
Krupal <krupalpatel1998@gmail.com>
2018-10-26 12:31:34 -0600

    Made Main Function

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,9 +1,32 @@
 
-public class LinkedList {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class LinkedList{
+	private listElement leHead;
+	private listElement leTail;
+	private listElement le;
+	int elementCount;
 
-	}
+	public static void main(String[] args)
+	// TODO Auto-generated method stub
+	{
+		LinkedList SingleLL=new LinkedList();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Functions to Perform:");
+		System.out.println("Add");
+		System.out.println("Delete");
+		System.out.println("Retrieve");
+		System.out.println("Print From :");
+		System.out.println("Head");
+		System.out.println("Tail");
+		System.out.println("Do you want to exit? ");
+		System.out.println("yes");
+		String action="";
+
+		
+
+
+
+
 
 }
--- src/listElement.java
+++ src/listElement.java
@@ -1,4 +1,31 @@
 
 public class listElement {
+	public listElement next;
+	//private ListEl
+	private int data;
+	
+	public listElement()
+	{
+		this.next = null;
+		this.data = 0;
+		
+	}
+	
+	public void setNext(listElement le)
+	{
+		this.next=le;
+	}
+	public listElement getNext()
+	{
+		return this.next;
+	}
+	public void setData(int data)
+	{
+		this.data = data;
+	}
 
+	public int getData()
+	{
+		return this.data;
+	}
 }
